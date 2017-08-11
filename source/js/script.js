a883eca08009c0d38f7e4dc696dd8235e05a41d8
Monica Salter <monica@gravityworksdesign.com>
2017-08-11 12:32:00 -0700

    Added js button ripple effect

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,34 @@
 'use strict';
 
 /* 
- * Use an anonymous function for jQuery
- * Keeps it from breaking in Drupal especially
+ * !!! Change this for Drupal ¡¡¡
+ * Any script file in a Drupal theme, like Basic, should have the functions you need
  */
 (function($) {
   $(document).ready(function(){
+
+  		console.log('the script file is here');
+
+  		// Button Ripple
+		$('.js-ripple').on('click', function(e) {
+			console.log('you clicked a ripple button!');
+			var target = e.target;
+			var rect = target.getBoundingClientRect();
+			var ripple = target.querySelector('.ripple');
+			if (!ripple) {
+			    ripple = document.createElement('span');
+			    ripple.className = 'ripple';
+			    ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+			    target.appendChild(ripple);
+			}
+			ripple.classList.remove('show');
+			var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+			var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+			ripple.style.top = top + 'px';
+			ripple.style.left = left + 'px';
+			ripple.classList.add('show');
+			return false;
+		});
 
 		// Get IE Version
 		var version = detectIE();
